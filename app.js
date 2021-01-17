49d6c5a3ff68279f87de8b5ee82b20f8520ec883
arfinfoysalbd <68894408+arfinfoysalbd@users.noreply.github.com>
2021-01-17 16:13:52 +0600

    Html
    Html File

---
+++ app.js
@@ -0,0 +1,35 @@
+// var fancy= 56;
+
+// console.log(fancy);
+
+// let arfin;
+// arfin= " foysal";
+// let foysal= 55;
+// let dd= 56;
+
+// console.log( foysal+dd);
+
+// console.log( arfin);
+
+// // let arfinFoysal = ( " tom and jerry");
+
+// // console.log arfinFoysal;
+
+// const ff=[ 'foysal','tmi','jumi',55,60]
+// ff.push ( "pagla");
+// // ff.pop ()
+ 
+// console.log( ff);
+
+var arfin= 77;
+
+console.log(arfin);
+
+// const foysal = "this is our bangladesg";
+
+// console.log( foysal);
+
+const baby=  [ " sweet, bow " ,555, 44];
+baby.push ( " 55")
+baby.pop ()
+console.log( baby);
